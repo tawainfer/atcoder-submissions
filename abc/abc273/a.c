7beb82e21280981caf7b1be6c4fe3602752efb20
tawainfer <tawainfer@gmail.com>
2023-07-02 09:33:24 +0900

    2件のファイルを更新しました。

---
+++ abc/abc273/a.c
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/abc273/submissions/35710853
+
+#include <stdio.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  
+  int ans = 1;
+  for(int i = 1; i <= n; i++) ans *= i;
+  
+  printf("%d", ans);
+  return 0;
+}
