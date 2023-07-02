990c630114549f66f0f0737257374281fac1553e
tawainfer <tawainfer@gmail.com>
2023-07-03 06:51:34 +0900

    2件のファイルを更新しました。

---
+++ arc/arc163/a.py
@@ -0,0 +1,25 @@
+# https://atcoder.jp/contests/arc163/submissions/43192411
+
+t = int(input())
+
+for _ in range(t):
+  n = int(input())
+  s = input()
+
+  ptn = []
+  u = s[0]
+
+  check = False
+  for i in range(1, n):
+    if s[0] < s[i]:
+      check = True
+    elif s[0] == s[i] and s[:i] < s[i:]:
+      check = True
+
+  if check:
+    print('Yes')
+    # continue
+  else:
+    print('No')
+
+  
