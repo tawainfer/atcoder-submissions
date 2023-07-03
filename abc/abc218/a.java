540bb836a58428b2ef76d2a7781fc015f5e96b1f
tawainfer <tawainfer@gmail.com>
2023-07-03 18:40:58 +0900

    7件のファイルを更新しました。

---
+++ abc/abc218/a.java
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/abc218/submissions/43221890
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner stdIn = new Scanner(System.in);
+
+    int n = stdIn.nextInt() - 1;
+    String s = stdIn.next();
+
+    System.out.println(s.charAt(n) == 'o' ? "Yes" : "No");
+  }
+}
---
+++ abc/abc291/a.java
@@ -0,0 +1,19 @@
+// https://atcoder.jp/contests/abc291/submissions/43222000
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner stdIn = new Scanner(System.in);
+
+    String s = stdIn.next();
+    int n = s.length();
+
+    for(int i = 0; i < n; i++) {
+      if(s.charAt(i) <= 'Z') {
+        System.out.println(i + 1);
+        break;
+      }
+    }
+  }
+}
