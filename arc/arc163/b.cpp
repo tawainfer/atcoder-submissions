7b76da7c8e9fb785d7030586862dbc493e1eb7c1
tawainfer <tawainfer@gmail.com>
2023-07-08 15:47:12 +0900

    1件のファイルを更新しました。

--- arc/arc163/b.cpp
+++ arc/arc163/b.cpp
@@ -1,4 +1,4 @@
-// https://atcoder.jp/contests/arc163/submissions/43322714
+// https://atcoder.jp/contests/arc163/submissions/43329773
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -12,7 +12,7 @@ int main() {
   cin >> l >> r;
 
   vector<int> a(n - 2);
-  for(int &z : a) cin >> z;
+  for(int &x : a) cin >> x;
   sort(a.begin(), a.end());
 
   int ans = INF;
