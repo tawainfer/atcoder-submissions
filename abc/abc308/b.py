12f437005f59ce1615f9dd2580b945906007bb80
tawainfer <tawainfer@gmail.com>
2023-07-08 11:14:51 +0900

    3件のファイルを更新しました。

---
+++ abc/abc308/b.py
@@ -0,0 +1,19 @@
+# https://atcoder.jp/contests/abc308/submissions/43322802
+
+n, m = map(int, input().split())
+c = input().split()
+d = input().split()
+p = list(map(int, input().split()))
+
+dish_price = dict()
+for i in range(m):
+  dish_price[d[i]] = p[i + 1]
+
+ans = 0
+for s in c:
+  if s in dish_price:
+    ans += dish_price[s]
+  else:
+    ans += p[0]
+
+print(ans)
---
+++ arc/arc163/b.cpp
@@ -0,0 +1,29 @@
+// https://atcoder.jp/contests/arc163/submissions/43322714
+
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 2e9;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  int l, r;
+  cin >> l >> r;
+
+  vector<int> a(n - 2);
+  for(int &z : a) cin >> z;
+  sort(a.begin(), a.end());
+
+  int ans = INF;
+  for(int i = 0; i < a.size() - m + 1; i++) {
+    int j = i + m - 1;
+    int cnt = 0;
+    cnt += max(l - a[i], 0);
+    cnt += max(a[j] - r, 0);
+    ans = min(ans, cnt);
+  }
+
+  cout << ans;
+  return 0;
+}
