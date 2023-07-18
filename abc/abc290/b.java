08278b82100497b857dfd1055bab483f7c1d9aec
tawainfer <tawainfer@gmail.com>
2023-07-18 13:36:51 +0900

    1件のファイルを更新しました。

---
+++ abc/abc290/b.java
@@ -0,0 +1,20 @@
+// https://atcoder.jp/contests/abc290/submissions/43714154
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+
+    int n = stdIn.nextInt();
+    int k = stdIn.nextInt();
+    String s = stdIn.next();
+
+    int cnt = 0;
+    for(int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      if(c == 'o') cnt++;
+      System.out.print(c == 'o' && cnt <= k ? 'o' : 'x');
+    }
+  }
+}
