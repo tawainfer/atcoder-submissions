43cdd23e3153eb67408b5d7079a994a00e014457
tawainfer <tawainfer@gmail.com>
2023-07-19 08:03:33 +0900

    1件のファイルを更新しました。

---
+++ abc/abc167/a.java
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc167/submissions/43735309
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner stdIn = new Scanner(System.in);
+
+    String s = stdIn.next();
+    String t = stdIn.next();
+
+    for(int i = 0; i < s.length(); i++) {
+      if(s.charAt(i) != t.charAt(i)) {
+        System.out.println("No");
+        System.exit(0);
+      }
+    }
+
+    System.out.println("Yes");
+  }
+}
