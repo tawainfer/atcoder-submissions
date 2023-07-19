8c6ff74e677d9d036126f0aafb90666cdf24ac0a
tawainfer <tawainfer@gmail.com>
2023-07-20 07:37:08 +0900

    2件のファイルを更新しました。

---
+++ abc/abc165/a.java
@@ -0,0 +1,22 @@
+// https://atcoder.jp/contests/abc165/submissions/43760855
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner stdIn = new Scanner(System.in);
+
+    int k = stdIn.nextInt();
+    int a = stdIn.nextInt();
+    int b = stdIn.nextInt();
+
+    for(int i = a; i <= b; i++) {
+      if(i % k == 0) {
+        System.out.println("OK");
+        System.exit(0);
+      }
+    }
+
+    System.out.println("NG");
+  }
+}
