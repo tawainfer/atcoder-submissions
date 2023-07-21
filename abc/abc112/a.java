cf001c1ef5b1de3f82f40e81f37aa13c638205b6
tawainfer <tawainfer@gmail.com>
2023-07-22 05:04:29 +0900

    2件のファイルを更新しました。

---
+++ abc/abc112/a.java
@@ -0,0 +1,19 @@
+// https://atcoder.jp/contests/abc112/submissions/43810426
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner stdIn = new Scanner(System.in);
+
+    int n = stdIn.nextInt();
+
+    if(n == 1) {
+      System.out.println("Hello World");
+    } else {
+      int a = stdIn.nextInt();
+      int b = stdIn.nextInt();
+      System.out.println(a + b);
+    }
+  }
+}
