304bdb5cd0f47f87e7b6f5a04568857e170bd3bc
tawainfer <tawainfer@gmail.com>
2023-07-21 12:38:00 +0900

    1件のファイルを更新しました。

---
+++ abc/abc238/a.java
@@ -0,0 +1,16 @@
+// https://atcoder.jp/contests/abc238/submissions/43793241
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+
+    int n = stdIn.nextInt();
+    if(n == 1 || n > 4) {
+      System.out.println("Yes");
+    } else {
+      System.out.println("No");
+    }
+  }
+}
