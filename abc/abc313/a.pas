c7589acee10f4312a1f5d854cf2037f341425b8d
tawainfer <tawainfer@gmail.com>
2023-08-10 11:49:31 +0900

    1件のファイルを更新しました。

---
+++ abc/abc313/a.pas
@@ -0,0 +1,31 @@
+// https://atcoder.jp/contests/abc313/submissions/44425635
+
+var
+  n, i, ma, countMa: integer;
+  p: array of integer;
+
+begin
+  readln(n);
+  SetLength(p, n);
+  for i := 0 to n - 1 do
+    read(p[i]);
+
+  ma := p[0];
+  countMa := 0;
+  for i := 0 to n - 1 do
+  begin
+    if p[i] > ma then
+    begin
+      ma := p[i];
+      countMa := 1;
+    end
+    else if p[i] = ma then
+      countMa := countMa + 1;
+  end;
+
+  if (p[0] = ma) and (countMa = 1) then
+    writeln(0)
+  else
+    writeln(ma - p[0] + 1);
+end.
+
