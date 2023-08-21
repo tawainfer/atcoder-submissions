91b5d199384184bab48a9945293f140c204c4499
tawainfer <tawainfer@gmail.com>
2023-08-21 16:40:50 +0900

    1件のファイルを更新しました。

---
+++ abc/abc229/e.cpp
@@ -0,0 +1,115 @@
+// https://atcoder.jp/contests/abc229/submissions/44817688
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class UnionFind
+{
+public:
+
+	UnionFind() = default;
+
+	/// @brief Union-Find 木を構築します。
+	/// @param n 要素数
+	explicit UnionFind(size_t n)
+		: m_parents(n)
+		, m_sizes(n, 1)
+	{
+		std::iota(m_parents.begin(), m_parents.end(), 0);
+	}
+
+	/// @brief 頂点 i の root のインデックスを返します。
+	/// @param i 調べる頂点のインデックス
+	/// @return 頂点 i の root のインデックス
+	int find(int i)
+	{
+		if (m_parents[i] == i)
+		{
+			return i;
+		}
+
+		// 経路圧縮
+		return (m_parents[i] = find(m_parents[i]));
+	}
+
+	/// @brief a のグループと b のグループを統合します。
+	/// @param a 一方のインデックス
+	/// @param b 他方のインデックス
+	void merge(int a, int b)
+	{
+		a = find(a);
+		b = find(b);
+
+		if (a != b)
+		{
+			// union by size (小さいほうが子になる）
+			if (m_sizes[a] < m_sizes[b])
+			{
+				std::swap(a, b);
+			}
+
+			m_sizes[a] += m_sizes[b];
+			m_parents[b] = a;
+		}
+	}
+
+	/// @brief a と b が同じグループに属すかを返します。
+	/// @param a 一方のインデックス
+	/// @param b 他方のインデックス
+	/// @return a と b が同じグループに属す場合 true, それ以外の場合は false
+	bool connected(int a, int b)
+	{
+		return (find(a) == find(b));
+	}
+
+	/// @brief i が属するグループの要素数を返します。
+	/// @param i インデックス
+	/// @return i が属するグループの要素数
+	int size(int i)
+	{
+		return m_sizes[find(i)];
+	}
+
+private:
+
+	// m_parents[i] は i の 親,
+	// root の場合は自身が親
+	std::vector<int> m_parents;
+
+	// グループの要素数 (root 用)
+	// i が root のときのみ, m_sizes[i] はそのグループに属する要素数を表す
+	std::vector<int> m_sizes;
+};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<vector<int>> p(n + 1);
+  while(m--) {
+    int a, b;
+    cin >> a >> b;
+    p[a].push_back(b);
+  }
+
+  UnionFind uf(n + 1);
+  vector<int> ans = {0, 1};
+  for(int cp = n - 1; cp >= 1; cp--) {
+    set<int> se;
+    for(int ep : p[cp]) {
+      se.insert(uf.find(ep));
+    }
+
+    for(int ep : p[cp]) {
+      uf.merge(cp, ep);
+    }
+
+    ans.push_back((ans.back() + 1) - se.size());
+    // cout << cp << " " << se.size() <<  " " << ans.back() << endl;
+  }
+
+  ans.pop_back();
+  reverse(ans.begin(), ans.end());
+  for(int x : ans) cout << x << endl;
+  return 0;
+}
