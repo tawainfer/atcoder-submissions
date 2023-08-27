4c68963fd8cae6fb136c617718f263690a394a1e
tawainfer <tawainfer@gmail.com>
2023-08-27 13:46:24 +0900

    1件のファイルを更新しました。

---
+++ abc/abc261/a.java
@@ -0,0 +1,27 @@
+// https://atcoder.jp/contests/abc261/submissions/44994241
+
+import java.util.Scanner;
+
+class Main {
+  static int max(int x, int y) {
+    return (x > y ? x : y);
+  }
+
+  public static void main(String args[]) {
+    Scanner stdIn = new Scanner(System.in);
+
+    int l1 = stdIn.nextInt();
+    int r1 = stdIn.nextInt();
+    int l2 = stdIn.nextInt();
+    int r2 = stdIn.nextInt();
+
+    int cnt = 0;
+    for(int i = l1; i <= r1; i++) {
+      if(l2 <= i && i <= r2) {
+        cnt++;
+      }
+    }
+
+    System.out.println(max(cnt - 1, 0));
+  }
+}
