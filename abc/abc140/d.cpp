a3a6899d3be1436ba33d934f60c57ef1c622fa3c
tawainfer <tawainfer@gmail.com>
2023-08-27 15:15:07 +0900

    1件のファイルを更新しました。

---
+++ abc/abc140/d.cpp
@@ -0,0 +1,23 @@
+// https://atcoder.jp/contests/abc140/submissions/44995927
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  string s;
+  cin >> s;
+  s += '?';
+
+  int cnt = 0;
+  for(int i = s.size() - 2; i >= 0; i--) {
+    if(s[i] != s[i + 1]) {
+      cnt++;
+    }
+  }
+
+  cout << n - max(cnt - 2 * k, 1);
+  return 0;
+}
