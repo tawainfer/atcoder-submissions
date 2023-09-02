14bba0c0d23924825ed40e77d634385e468f68b9
tawainfer <tawainfer@gmail.com>
2023-09-02 11:32:40 +0900

    1件のファイルを更新しました。

---
+++ arc/arc067/b.cpp
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc052/submissions/45115662
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ll n, a, b;
+  cin >> n >> a >> b;
+
+  vector<ll> x(n);;
+  for(auto &z : x) cin >> z;
+
+  ll ans = 0;
+  for(int i = 1; i < n; i++) {
+    ans += min((x[i] - x[i - 1]) * a, b);
+  }
+
+  cout << ans;
+  return 0;
+}
