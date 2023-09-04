187d272219013b774bcce921284a1034546365d7
tawainfer <tawainfer@gmail.com>
2023-09-04 17:37:31 +0900

    1件のファイルを更新しました。

---
+++ abc/abc152/d.cpp
@@ -0,0 +1,26 @@
+// https://atcoder.jp/contests/abc152/submissions/45240779
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ll n;
+  cin >> n;
+
+  vector<vector<int>> cnt(10, vector<int>(10, 0));
+  for(int i = 1; i <= n; i++) {
+    string s = to_string(i);
+    cnt[s.front() - '0'][s.back() - '0']++;
+  }
+
+  ll ans = 0;
+  for(int i = 1; i <= n; i++) {
+    string s = to_string(i);
+    ans += cnt[s.back() - '0'][s.front() - '0'];
+  }
+
+  cout << ans;
+  return 0;
+}
+
