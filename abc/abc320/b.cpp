bb90d32cedbcb1c209278ee17648c4a006ea5ef6
tawainfer <tawainfer@gmail.com>
2023-09-16 23:32:15 +0900

    2件のファイルを更新しました。

---
+++ abc/abc320/b.cpp
@@ -0,0 +1,22 @@
+// https://atcoder.jp/contests/abc320/submissions/45632435
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+
+  int ans = 0;
+  for(int i = 0; i < s.size(); i++) {
+    for(int j = i; j < s.size(); j++) {
+      string t = s.substr(i, j - i + 1);
+      string u = t;
+      reverse(u.begin(), u.end());
+      if(t == u) ans = max(ans, (int)t.size());
+    }
+  }
+
+  cout << ans;
+  return 0;
+}
