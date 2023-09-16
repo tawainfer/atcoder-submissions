5d89d2e72632177ade2651cbb136e80ab728605f
tawainfer <tawainfer@gmail.com>
2023-09-16 23:52:36 +0900

    2件のファイルを更新しました。

---
+++ abc/abc320/b.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc320/submissions/45587930
+
+s = input()
+ans = 0
+
+for i in range(len(s)):
+  for j in range(i, len(s)):
+    t = s[i:j+1]
+    u = t[::-1]
+    if t == u:
+      ans = max(ans, len(t))
+
+print(ans)
