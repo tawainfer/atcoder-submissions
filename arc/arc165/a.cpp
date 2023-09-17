e6337bd417d9291ce562fb73c19efe104fa9aeef
tawainfer <tawainfer@gmail.com>
2023-09-17 23:25:59 +0900

    1件のファイルを更新しました。

---
+++ arc/arc165/a.cpp
@@ -0,0 +1,48 @@
+// https://atcoder.jp/contests/arc165/submissions/45674531
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+template <typename T>
+vector<pair<T, T>> prime_factor(T n) {
+    vector<pair<T, T>> ret;
+    for (T i = 2; i * i <= n; i++) {
+        if (n % i != 0) continue;
+        T tmp = 0;
+        while (n % i == 0) {
+            tmp++;
+            n /= i;
+        }
+        ret.push_back(make_pair(i, tmp));
+    }
+    if (n != 1) ret.push_back(make_pair(n, 1));
+    return ret;
+}
+
+int main() {
+  ll t;
+  cin >> t;
+
+  for(int i = 0; i < t; i++) {
+    ll n;
+    cin >> n;
+
+    vector<pair<ll, ll>> res = prime_factor(n);
+    ll sum = 0;
+    ll cnt = 0;
+    for(auto p : res) {
+      cnt++;
+      sum += p.first;
+      // cout << p.first << " " << p.second << endl;
+    }
+    // cout << endl;
+
+    // cout << "sum=" << sum << endl;
+    if(sum <= n && cnt >= 2) {
+      cout << "Yes\n";
+    } else {
+      cout << "No\n";
+    }
+  }
+}
