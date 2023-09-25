07ecce39bc3f00fee62e77d66aa556facf40d850
tawainfer <tawainfer@gmail.com>
2023-09-25 23:33:21 +0900

    1件のファイルを更新しました。

---
+++ abc/abc307/b.java
@@ -0,0 +1,45 @@
+// https://atcoder.jp/contests/abc307/submissions/45929977
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String args[]) {
+    Scanner stdIn = new Scanner(System.in);
+
+    int n = stdIn.nextInt();
+    String[] s = new String[n];
+    for(int i = 0; i < n; i++) {
+      s[i] = stdIn.next();
+    }
+
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < n; j++) {
+        if(i == j) continue;
+
+        char[] t = new char[s[i].length() + s[j].length()];
+        for(int k = 0; k < s[i].length(); k++) {
+          t[k] = s[i].charAt(k);
+        }
+
+        for(int k = 0; k < s[j].length(); k++) {
+          t[s[i].length() + k] = s[j].charAt(k);
+        }
+
+        boolean check = true;
+        for(int k = 0; k < t.length / 2; k++) {
+          if(t[k] != t[t.length - 1 - k]) {
+            check = false;
+            break;
+          }
+        }
+
+        if(check) {
+          System.out.println("Yes");
+          System.exit(0);
+        }
+      }
+    }
+
+    System.out.println("No");
+  }
+}
