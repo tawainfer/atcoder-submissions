524558f3a051f6c7339ee4d700b08d4d5d86603b
tawainfer <tawainfer@gmail.com>
2023-10-21 23:32:49 +0900

    5件のファイルを更新しました。

---
+++ abc/abc325/a.cpp
@@ -0,0 +1,11 @@
+// https://atcoder.jp/contests/abc325/submissions/46791143
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+
+  cout << s << " san";
+}
---
+++ abc/abc325/b.cpp
@@ -0,0 +1,30 @@
+// https://atcoder.jp/contests/abc325/submissions/46798904
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<int> cnt(24, 0);
+  for(int i = 0; i < n; i++) {
+    int w, x;
+    cin >> w >> x;
+    cnt[x] += w;
+  }
+
+  int ans = 0;
+  for(int i = 0; i < 24; i++) {
+    int c = 0;
+    for(int j = 0; j < 9; j++) {
+      int t = (i + j) % 24;
+      c += cnt[t];
+    }
+
+    ans = max(ans, c);
+  }
+
+
+  cout << ans;
+}
