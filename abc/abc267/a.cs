a50cec0507f6e53dcf0e6335794c553c9020e222
tawainfer <tawainfer@gmail.com>
2023-10-24 23:32:45 +0900

    4件のファイルを更新しました。

---
+++ abc/abc267/a.cs
@@ -0,0 +1,20 @@
+// https://atcoder.jp/contests/abc267/submissions/46890016
+
+class A
+{
+  static void Main()
+  {
+    string s = Console.ReadLine();
+    string[] ptn = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+
+    for(int i = 0; i < 5; i++)
+    {
+      if(s == ptn[i])
+      {
+        Console.Write(5 - i);
+        return;
+      }
+    }
+  }
+}
+
---
+++ abc/abc276/a.cs
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc276/submissions/46889982
+
+class A
+{
+  static void Main()
+  {
+    string s = Console.ReadLine();
+    int ans = -1;
+
+    for(int i = 0; i < s.Length; i++)
+    {
+      if(s[i] == 'a')
+      {
+        ans = i + 1;
+      }
+    }
+
+    Console.Write(ans);
+  }
+}
+
