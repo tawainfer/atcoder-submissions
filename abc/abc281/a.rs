e65673859cf58fef77feda22c535cadb0451dd9f
tawainfer <tawainfer@gmail.com>
2023-11-13 23:32:41 +0900

    1件のファイルを更新しました。

---
+++ abc/abc281/a.rs
@@ -0,0 +1,14 @@
+// https://atcoder.jp/contests/abc281/submissions/47543021
+
+use proconio::input;
+
+fn main() {
+  input! {
+    mut n: i8
+  }
+  
+  while n >= 0 {
+    println!("{}", n);
+    n -= 1;
+  }
+}
