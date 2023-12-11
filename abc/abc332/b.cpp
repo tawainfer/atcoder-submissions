2469e266d181c4ed20c95ad90f3e25b05cbdc895
tawainfer <tawainfer@gmail.com>
2023-12-11 23:32:45 +0900

    2件のファイルを更新しました。

---
+++ abc/abc332/b.cpp
@@ -0,0 +1,31 @@
+// https://atcoder.jp/contests/abc332/submissions/48372511
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int k, g, m;
+  cin >> k >> g >> m;
+
+  int cg = 0;
+  int cm = 0;
+  while(k--) {
+    if(cg == g) {
+      cg = 0;
+    } else if(cm == 0) {
+      cm = m;
+    } else {
+      if(cg + cm > g) {
+        cm -= g - cg;
+        cg = g;
+      } else {
+        cg += cm;
+        cm = 0;
+      }
+    }
+
+  // cout << cg << " " << cm << endl;
+  }
+
+  cout << cg << " " << cm;
+}
