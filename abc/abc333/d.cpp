70f334467acd9522d005eaf6e485d1e394c162a3
tawainfer <tawainfer@gmail.com>
2023-12-17 23:32:53 +0900

    5件のファイルを更新しました。

---
+++ abc/abc333/d.cpp
@@ -0,0 +1,63 @@
+// https://atcoder.jp/contests/abc333/submissions/48570355
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void dfs() {
+
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<set<int>> g(n + 1);
+  for(int i = 0; i < n - 1; i++) {
+    int u, v;
+    cin >> u >> v;
+    g[u].insert(v);
+    g[v].insert(u);
+  }
+
+  if(g[1].size() == 1) {
+    cout << 1;
+    return 0;
+  }
+
+  vector<bool> seen(n + 1, false);
+  seen[1] = true;
+
+  vector<int> ptn;
+  for(int sp : g[1]) {
+    int cnt = 0;
+    queue<int> q;
+    q.push(sp);
+    seen[sp] = true;
+    // cout << 1 << " " << sp << endl;
+
+    while(!q.empty()) {
+      int cp = q.front();
+      q.pop();
+      cnt++;
+
+      for(auto ep : g[cp]) {
+        if(!seen[ep]) {
+          seen[ep] = true;
+          // cout << cp << " " << ep << endl;
+          q.push(ep);
+        }
+      }
+    }
+
+    ptn.push_back(cnt);
+  }
+
+  int ans = 1;
+  sort(ptn.begin(), ptn.end());
+  ptn.pop_back();
+  for(auto x : ptn) {
+    ans += x;
+  }
+
+  cout << ans;
+}
---
+++ abc/abc333/e.cpp
@@ -0,0 +1,55 @@
+// https://atcoder.jp/contests/abc333/submissions/48583696
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ll n;
+  cin >> n;
+
+  vector<set<ll>> pos(n + 1);
+  vector<ll> t(n), x(n);
+  for(int i = 0; i < n; i++) {
+    cin >> t[i] >> x[i];
+    if(t[i] == 1) {
+      pos[x[i]].insert(i);
+    }
+  }
+
+  vector<ll> ans(n, 0);
+  for(int i = n - 1; i >= 0; i--) {
+    if(t[i] == 2) {
+      auto iter = pos[x[i]].lower_bound(i);
+      if(iter == pos[x[i]].begin()) {
+        cout << "-1";
+        return 0;
+      }
+
+      iter--;
+      ans[(*iter)]++;
+      pos[x[i]].erase(iter);
+    }
+  }
+
+  ll cnt = 0;
+  multiset<ll> ms;
+  for(int i = 0; i < n; i++) {
+    if(t[i] == 1 && ans[i] == 1) {
+      ms.insert(x[i]);
+      // cout << "add " << x[i] << endl;
+    } else if(t[i] == 2) {
+      ms.erase(ms.find(x[i]));
+      // cout << "erase " << x[i] << endl;
+    }
+
+    cnt = max(cnt, (ll)ms.size());
+  }
+
+  cout << cnt << endl;
+  for(int i = 0; i < n; i++) {
+    if(t[i] == 1) {
+      cout << ans[i] << " ";
+    }
+  }
+}
