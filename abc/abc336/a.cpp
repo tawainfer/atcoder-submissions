404f9eeb276a14e8661e3053bb6297595016fc5b
tawainfer <tawainfer@gmail.com>
2024-01-15 23:32:48 +0900

    3件のファイルを更新しました。

---
+++ abc/abc336/a.cpp
@@ -0,0 +1,15 @@
+// https://atcoder.jp/contests/abc336/submissions/49278108
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  cout << "L";
+  for(int i = 0; i < n; i++) {
+    cout << "o";
+  }
+  cout << "ng";
+}
