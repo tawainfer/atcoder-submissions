ec641267a96720c3f90f031aadbddc188221c8bf
tawainfer <tawainfer@gmail.com>
2024-01-30 11:11:33 +0900

    1件のファイルを更新しました。

---
+++ abc/abc338/b.cs
@@ -0,0 +1,35 @@
+// https://atcoder.jp/contests/abc338/submissions/49801943
+
+using static System.Console;
+
+public class A
+{
+  static void Main()
+  {
+    string s = ReadLine();
+    int[] cnt = new int[26];
+    for(int i = 0; i < cnt.Length; i++)
+    {
+      cnt[i] = 0;
+    }
+
+    foreach(char c in s)
+    {
+      cnt[c - 'a']++;
+    }
+
+    int max_cnt = 0;
+    char ans = '?';
+    for(int i = 0; i < cnt.Length; i++)
+    {
+      if(max_cnt < cnt[i])
+      {
+        ans = (char)('a' + i);
+        max_cnt = cnt[i];
+      }
+    }
+
+    Write(ans);
+  }
+}
+
