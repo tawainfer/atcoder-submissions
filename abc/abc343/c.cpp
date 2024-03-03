bc0f00af4b0acdb2804cfb7fd4e7a189743f3f08
tawainfer <tawainfer@gmail.com>
2024-03-03 09:14:06 +0900

    4件のファイルを更新しました。

---
+++ abc/abc343/c.cpp
@@ -0,0 +1,25 @@
+// https://atcoder.jp/contests/abc343/submissions/50785643
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const ll MAX = 1000000;
+
+int main() {
+  ll n;
+  cin >> n;
+
+  vector<ll> v;
+  for(ll i = 1; i <= MAX; i++) {
+    string s = to_string(i * i * i);
+    string t = s;
+    reverse(t.begin(), t.end());
+    if(s == t) {
+      v.push_back(i * i * i);
+    }
+  }
+
+  auto iter = lower_bound(v.begin(), v.end(), n + 1);
+  iter--;
+  cout << (*iter);
+}
---
+++ abc/abc343/d.cpp
@@ -0,0 +1,37 @@
+// https://atcoder.jp/contests/abc343/submissions/50798772
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+  ll n, t;
+  cin >> n >> t;
+
+  // set<int> se;
+  // se.insert(0);
+  map<ll, ll> m;
+  m[0] = n;
+  ll cnt = 1;
+  
+  vector<ll> p(n + 1, 0);
+  for(int i = 0; i < t; i++) {
+    ll a, b;
+    cin >> a >> b;
+
+    m[p[a]]--;
+    if(m[p[a]] == 0) {
+      // se.erase(p[a]);
+      cnt--;
+    }
+
+    p[a] += b;
+    if(m[p[a]] == 0) {
+      cnt++;
+      // se.insert(p[a]);
+    }
+    m[p[a]]++;
+
+    cout << cnt << endl;
+  }
+}
