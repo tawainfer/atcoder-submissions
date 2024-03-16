f245ae9733d7b82576d1b27f005623cb5af3b8cd
tawainfer <tawainfer@gmail.com>
2024-03-16 15:49:41 +0900

    2件のファイルを更新しました。

---
+++ abc/abc342/d.cpp
@@ -0,0 +1,47 @@
+// https://atcoder.jp/contests/abc342/submissions/51264270
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+ll f(ll x) {
+  for(ll i = 2; i * i <= x; i++) {
+    while(x % (i * i) == 0) {
+      x /= i * i;
+    }
+  }
+
+  return x;
+}
+
+int main() {
+  ll n;
+  cin >> n;
+
+  vector<int> a(n);
+  for(auto &x : a) cin >> x;
+
+  map<ll, ll> m;
+  ll cnt = 0;
+
+  for(auto x : a) {
+    if(x == 0) {
+      cnt++;
+    } else {
+      m[f(x)]++;
+    }
+  }
+
+  ll ans = 0;
+  for(int i = 0; i < n; i++) {
+    if(a[i] == 0) {
+      ans += n - i - 1;
+      cnt--;
+    } else {
+      ans += m[f(a[i])] - 1 + cnt;
+      m[f(a[i])]--;
+    }
+  }
+
+  cout << ans;
+}
