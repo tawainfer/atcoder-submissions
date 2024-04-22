5dbe0b55e18463b163498702e70783789120f5bb
tawainfer <tawainfer@gmail.com>
2024-04-22 16:50:25 +0900

    1件のファイルを更新しました。

--- abc/abc350/c.cpp
+++ abc/abc350/c.cpp
@@ -1,4 +1,4 @@
-// https://atcoder.jp/contests/abc350/submissions/52572863
+// https://atcoder.jp/contests/abc350/submissions/52686551
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,7 +8,7 @@ int main() {
   ll n;
   cin >> n;
 
-  map<ll, ll> m; // idx, pos
+  map<ll, ll> m;
   vector<ll> a(n);
   for(int i = 0; i < n; i++) {
     cin >> a[i];
@@ -18,20 +18,11 @@ int main() {
   vector<vector<ll>> ans;
   for(ll i = 0; i < n; i++) {
     if(m[i + 1] != i) {
-      // cout << min(i, m[i + 1]) << " " << max(i, m[i + 1]) << endl;
       ans.push_back({min(i, m[i + 1]), max(i, m[i + 1])});
-      // int t = m[a[i]];
-      // m[a[i]] = m[i + 1];
-      // swap(m[a[i]], m[i + 1]);
       m[a[i]] = m[i + 1];
       swap(a[i], a[m[i + 1]]);
       m[i + 1] = i;
     }
-
-    // for(auto x : a) {
-    //   cout << x << " ";
-    // }
-    // cout << endl;
   }
 
   cout << ans.size() << endl;
