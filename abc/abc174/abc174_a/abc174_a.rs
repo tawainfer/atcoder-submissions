b9ed9adf9d0b93a153d84d6ffeebd668f2f25f8b
tawainfer <tawainfer@gmail.com>
2024-05-31 09:51:40 +0900

    update: 2024-05-31 09:51:40 +0900

--- abc/abc174/abc174_a/abc174_a.rs
+++ abc/abc174/abc174_a/abc174_a.rs
@@ -1,9 +1,9 @@
 use proconio::input;
 
 fn main() {
-    input! {
-        x: i8
-    }
-    if x >= 30 {print!("Yes")}
-    else {print!("No")}
+  input! {
+    x: i8,
+  }
+
+  print!("{}", if x >= 30 {"Yes"} else {"No"});
 }
