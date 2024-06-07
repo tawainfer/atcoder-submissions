4b4666a148bd6eb3933d62daf8ff7894c30d12c6
tawainfer <tawainfer@gmail.com>
2024-06-07 12:33:42 +0900

    update: 2024-06-07 12:33:42 +0900

--- abc/abc170/abc170_a/abc170_a.rs
+++ abc/abc170/abc170_a/abc170_a.rs
@@ -1,24 +1,10 @@
 use proconio::input;
 
 fn main() {
-    input! {
-        a: i32,
-        b: i32,
-        c: i32,
-        d: i32,
-        e: i32,
-    }
-  
-    let ans = if a == 0 {
-        1
-    } else if b == 0 {
-        2
-    } else if c == 0 {
-        3
-    } else if d == 0 {
-        4
-    } else {
-        5
-    };
-    print!("{}", ans);
+  input! {
+      a: [u8; 5],
+  }
+
+  print!("{}", 15 - a.iter().sum::<u8>());
 }
+
