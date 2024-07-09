78e2773cfa73ca302499027f049a7da12751cb6e
tawainfer <tawainfer@gmail.com>
2024-07-09 21:45:41 +0900

    update: 2024-07-09 21:45:41 +0900

---
+++ abc/abc360/abc360_a/abc360_a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  
+  if(s == "SMR" || s == "MSR" || s == "MRS") {
+    cout << "No";
+  } else {
+    cout << "Yes";
+  }
+}
